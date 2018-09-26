8ef051a8d1afe31b31631edb9982185aaef71cf5
Hari Prasad <hprasad@sapient.com>
2018-09-26 11:56:29 +0530

    list user changed

--- src/components/ListUsers.jsx
+++ src/components/ListUsers.jsx
@@ -38,38 +38,38 @@ class ListUsers extends Component {
 }
 
 class WhosOnlineListItem extends Component {
-  render() {
-    const styles = {
-      li: {
-        display: 'flex',
-        alignItems: 'center',
-        marginTop: 5,
-        marginBottom: 5,
-        paddingTop: 2,
-        paddingBottom: 2,
-		paddingLeft: 0
-      },
-      div: {
-        borderRadius: '50%',
-        width: 11,
-        height: 11,
-        marginRight: 10,
-      },
-    }
-    return (
-     	<ListItem  style={styles.li}>
-        <div
-          style={{
-            ...styles.div,
-            backgroundColor:
-              this.props.presenceState === 'online' ? '#00FF00' : '#a9a9a9',
-          }}
-        />
-        {this.props.children}
-		</ListItem>
-      
-    )
-  }
+	render() {
+		const styles = {
+			li: {
+				display: 'flex',
+				alignItems: 'center',
+				marginTop: 5,
+				marginBottom: 5,
+				paddingTop: 2,
+				paddingBottom: 2,
+				paddingLeft: 0
+			},
+			div: {
+				borderRadius: '50%',
+				width: 11,
+				height: 11,
+				marginRight: 10
+			},
+		}
+		return (
+			<ListItem  style={styles.li}>
+			<div
+			  style={{
+				...styles.div,
+				backgroundColor:
+				  this.props.presenceState === 'online' ? '#00FF00' : '#a9a9a9',
+			  }}
+			/>
+			{this.props.children}
+			</ListItem>
+		  
+		)
+	}
 }
 
 export default ListUsers;
